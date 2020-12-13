ffb8f98f93f3874acd4978afd7c9edec56bf4061
William <Billy.campbell17@gmail.com>
2020-12-12 19:15:32 -0500

    updated Classes  & URLS

--- photonyc/urls.py
+++ photonyc/urls.py
@@ -2,16 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.collectionlist.as_view(), name='collectiton_list'),
-    path('photos/', views.photo_list, name='photo_list'),
+    path('', views.CollectionList.as_view(), name='collection_list'),
+    path('photos/', views.PhotoList.as_view(), name='photo_list'),
     path('collections/<int:pk>', views.collection_detail, name='collection_detail'),
-    path('photos/<int:pk>', views.photo_detail, name='photo_detail'),
-    path('collections/new', views.collection_create, name='collection_create'),
-    path('photos/new', views.photo_create, name='photo_create'),
+    path('photo/<int:pk>', views.PhotoDetail.as_view(), name='photo_detail'),
+    path('collection/new', views.CollectionCreate.as_view(),
+         name='collection_create'),
+    path('photos/new', views.PhotoCreate.as_view(), name='photo_create'),
     path('collections/<int:pk>/edit',
          views.collection_edit, name='collection_edit'),
     path('photos/<int:pk>/edit', views.photo_edit, name='photo_edit'),
-    path('collectons/<int:pk>/delete', views.collection_delete, name='collection_delete'),
+    path('collectons/<int:pk>/delete',
+         views.collection_delete, name='collection_delete'),
     path('photos/<int:pk>/delete', views.photo_delete, name='photo_delete'),
 
 ]
--- photonyc/views.py
+++ photonyc/views.py
@@ -2,17 +2,18 @@ from .models import Collection, Photo
 from django.shortcuts import render, redirect
 from .forms import CollectionForm, PhotoForm
 from django.views import View
+from django.views.generic import ListView, DetailView
 
 
-def CollectionList(View):
+class CollectionList(View):
     def get(self, request):
-    collection = Collection.objects.all()
-    return render(request, 'photo/collection_list.html', {'collections': collection})
+        collection = Collection.objects.all()
+        return render(request, 'photo/collection_list.html', {'collections': collection})
 
 
-def photo_list(request):
-    photos = Photo.objects.all()
-    return render(request, 'photo/photo_list.html', {'photos': photos})
+class PhotoList(ListView):
+    model = Photo
+    context_object_name = 'photo'
 
 
 def collection_detail(request, pk):
@@ -20,34 +21,42 @@ def collection_detail(request, pk):
     return render(request, 'photo/collection_detail.html', {'collection': collection})
 
 
-def photo_detail(request, pk):
-    photo = Photo.objects.get(id=pk)
-    return render(request, 'photo/photo_detail.html', {'photo': photo})
+class PhotoDetail(DetailView):
+    queryset = Photo.objects.all()
+    context_object_name = 'photo'
 
 
-def CollectionCreate(View):
+class CollectionCreate(View):
+    form_class = CollectionForm
+    template_name = 'photo/collection_form.html'
+
     def get(self, request):
-        form = CollectionForm()
-        return render(request, 'photo/collection_form.html', {'form': form})
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
         form = CollectionForm(request.POST)
         if form.is_valid():
             collection = form.save()
             return redirect('collection_detail', pk=collection.pk)
+        return render(request, self.template_name, {'form': form})
 
-        return render(request, 'photo/collection_form.html', {'form': form})
 
+class PhotoCreate(View):
+    form_class = PhotoForm
+    template_name = 'photo/photo_form.html'
 
-def photo_create(request):
-    if request.method == 'POST':
-        form = PhotoForm(request.POST)
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
         if form.is_valid():
             photo = form.save()
             return redirect('photo_detail', pk=photo.pk)
-    else:
-        form = PhotoForm()
-    return render(request, 'photo/photo_form.html', {'form': form})
+
+        return render(request, self.template_name, {'form': form})
 
 
 def collection_edit(request, pk):
